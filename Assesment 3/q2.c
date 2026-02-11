c7e58840dc89d18711856dd1b1367b03cefdfe79
nithin.c <nithinc.ec23@bitsathy.ac.in>
2026-02-11 14:04:06 +0530

    Create q2.c

---
+++ Assesment 3/q2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int n;
+    printf("Enter the number : ");
+    scanf("%d",&n);
+    if(n<=50){
+        printf("SUCCESS");
+    }
+    else {
+        printf("FAILURE");
+    }
+    
+}
