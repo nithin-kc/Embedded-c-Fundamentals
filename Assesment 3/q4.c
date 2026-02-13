c6c086dadd4734408b70c493a991de42b60a8e7c
nithin.c <nithinc.ec23@bitsathy.ac.in>
2026-02-13 09:25:23 +0530

    Create q4.c

---
+++ Assesment 3/q4.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+   int a=n/10;
+   int b=n%10;
+   if(a==b){
+       printf("Failure");
+   }
+   else{
+       printf("Sucess");
+   }
+}
