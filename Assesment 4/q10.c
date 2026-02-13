a1c33c73077b22da032245d19e94258354cbf05c
nithin.c <nithinc.ec23@bitsathy.ac.in>
2026-02-13 15:08:55 +0530

    Create q10.c

---
+++ Assesment 4/q10.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+    int n, sum =0,d;
+    scanf("%d",&n);
+    if (n<0){
+        n=-n;
+    }
+    while (n!=0) {
+        d= n% 10;
+        sum=sum+d;
+        n= n/ 10;
+    }
+    printf("%d", sum);
+}
