5729b2a6c376cb16e81ebd3030e7a2ad35218c0c
nithin.c <nithinc.ec23@bitsathy.ac.in>
2026-02-13 14:23:04 +0530

    Create q8.c

---
+++ Assesment 4/q8.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int n,d1,d2;
+    for(int i=10;i<=99;i++){
+        if(i%2==0){
+            d1=i/10;
+            d2=i%10;
+            int sum=d1+d2;
+            if(sum==6){
+                printf("%d\n",i);
+            }
+        }
+}
+}
